84be376b3dfe895ed135f6f21e1f50e21adbb771
Sanket Ughadmathe <59117184+sanketughadmathe@users.noreply.github.com>
2023-02-03 08:09:40 +0530

    New file

---
+++ 01 Loops/Pattern_11.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  /*
+  Printing:
+       1
+      22
+     333
+    4444
+  */
+
+  int n;
+  cout << "Enter the value of n: " << endl;
+  cin >> n;
+
+  int row = 1;
+
+  while (row <= n)
+  {
+
+    int col = 1;
+    int spaces = n - 1;
+
+    while (spaces >= row)
+    {
+      cout << " ";
+      spaces--;
+    }
+
+    while (col <= row)
+    {
+      cout << row;
+      col++;
+    }
+    cout << endl;
+    row++;
+  }
+}
